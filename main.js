c4ef1bd14a95db616b787a424f94105bb38edf6e
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-01-25 10:30:05 +0100

    chore: prepared url link, according to openweathermap documentation

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const photo = document.querySelector('.main-info__img')
 const weather = document.querySelector('.weather-info__weather')
 const temperature = document.querySelector('.weather-info__temperature')
 const humidity = document.querySelector('.weather-info__humidity')
+
+
+// Step by step 
+// Użwamy API  z openWeather.org 
+// API call mówi nam jak będziemy sie odwoływac
+// Tworzymy nasz apiLink dzięki któremu będziemy mogli sie odwoływać (appiCall) i tworzymy tez apiKey
+// Zmieniamy defoultowe jednostki na C (units=metric)
+// Nasz Link będzie wyglądał ApiLink +&ApiKey +&units
+
+const apiLink = 'api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=fa7a6052a41b9493569175c0523e195d';
+const units = '&units=metric'
+
+// Następnie tworzymy dwie zmienne city - będzie przechowywać to co wpisaliśmy ulr bedzie przechowywac nasz link 
+
+let city;
+let url;
+
+// Następnie tworzymy funkcję getWeather która będzie pobierać nasze dane z API
+
+const getWeather = () => {
+    city = input.nodeValue;
+    ulr = apiLink + city + apiKey + units;
+}
